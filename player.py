4e05cfbadc4e8fbe217e1ccd27c3702b79b0d7f1
karendepaz <78130251+karendepaz@users.noreply.github.com>
2022-07-14 22:21:09 -0500

    added doc string comment

--- player.py
+++ player.py
@@ -3,7 +3,18 @@ from point import Point
 from player_bullet import PlayerBullet
 
 class Player:
+    """
+    The Player class takes care of the players actions and attributes.
 
+    Attributes:
+        appearance
+        position
+        hit_points
+        move_counter
+        fire_counter
+        bullets
+    """
+    
     def __init__(self):
         self._appearance = "A"
         self._position = Point(450, 550)
